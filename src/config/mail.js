b463720d2199148c9973e43e5d44e357448b500c
Luiz Tonza + Funcional <ltonza@funcionalcorp.com.br>
2018-11-28 21:29:31 -0200

    avançando no módulo

---
+++ src/config/mail.js
@@ -0,0 +1,9 @@
+module.exports = {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '560ec2acfaa539',
+    pass: 'c6bb3c33088680'
+  }
+}
--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,26 @@ const express = require('express')
 
 const routes = express.Router()
 
-const UserController = require('./app/controllers/UserController')
+const authMiddleware = require('./app/middlewares/auth')
+const controllers = require('./app/controllers')
 
-routes.post('/users', UserController.store)
+routes.post('/users', controllers.UserController.store)
+routes.post('/sessions', controllers.SessionController.store)
+
+routes.use(authMiddleware)
+
+/**
+ * Ads
+ */
+routes.get('/ads', controllers.AdsController.index)
+routes.get('/ads/:id', controllers.AdsController.show)
+routes.post('/ads', controllers.AdsController.store)
+routes.put('/ads/:id', controllers.AdsController.update)
+routes.delete('/ads/:id', controllers.AdsController.destroy)
+
+/**
+ * Purchases
+ */
+routes.post('/purchases', controllers.Purchases.store)
 
 module.exports = routes
